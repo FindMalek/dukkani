51012e635c08bec6e129dfb94f10cc73073dd9aa
FindMalek <hi@findmalek.com>
2025-11-12 21:01:29 +0100

    perf: support '.env'

--- packages/db/prisma.config.ts
+++ packages/db/prisma.config.ts
@@ -1,10 +1,13 @@
 import path from "node:path";
 import type { PrismaConfig } from "prisma";
-import "./src/env";
+import { env } from "./src/env";
 
 export default {
 	schema: path.join("prisma", "schema"),
 	migrations: {
 		path: path.join("prisma", "migrations"),
+	},
+	datasource: {
+		url: env.DATABASE_URL,
 	},
 } satisfies PrismaConfig;
