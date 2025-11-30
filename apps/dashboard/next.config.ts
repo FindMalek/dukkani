eee72fe5e602607ce6885747450a837271528098
FindMalek <hi@findmalek.com>
2025-11-30 18:52:13 +0100

    fix: resolved 'next-intl' in 'dashboard'

--- apps/dashboard/next.config.ts
+++ apps/dashboard/next.config.ts
@@ -4,20 +4,9 @@ import createNextIntlPlugin from "next-intl/plugin";
 const withNextIntl = createNextIntlPlugin("./src/lib/i18n.ts");
 
 const nextConfig: NextConfig = {
-	typedRoutes: true,
 	reactCompiler: true,
 	transpilePackages: ["@dukkani/ui"],
 	serverExternalPackages: ["@prisma/client"],
-	redirects: async () => {
-		return [
-			{
-				source: "/",
-				// TODO: Change to default locale
-				destination: "/en/dashboard",
-				permanent: true,
-			},
-		];
-	},
 };
 
 export default withNextIntl(nextConfig);
