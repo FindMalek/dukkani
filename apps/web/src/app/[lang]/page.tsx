b393a6197434ae490e9b2e8c19eef2e03bb9d992
FindMalek <hi@findmalek.com>
2026-02-19 17:13:43 +0100

    feat: added 'pain' point section

--- apps/web/src/app/[lang]/page.tsx
+++ apps/web/src/app/[lang]/page.tsx
@@ -2,12 +2,14 @@ import { CTA } from "@/components/app/cta";
 import { FAQ } from "@/components/app/faq";
 import { Features } from "@/components/app/features";
 import { Hero } from "@/components/app/hero";
+import { PainPoints } from "@/components/app/pain-points";
 import { Pricing } from "@/components/app/pricing";
 
 export default function Home() {
 	return (
 		<>
 			<Hero />
+			<PainPoints />
 			<Features />
 			<Pricing />
 			<FAQ />
