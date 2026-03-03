87b08ab84e545a1d7967066a498819f0698fcdc6
Skandar Souissi <iskandarsouissi.0@gmail.com>
2026-03-03 16:58:32 +0100

    feat(product-form): enhance product form to support variant options

--- apps/dashboard/src/components/app/products/product-form.tsx
+++ apps/dashboard/src/components/app/products/product-form.tsx
@@ -4,7 +4,12 @@ import {
 	type CreateProductInput,
 	createProductInputSchema,
 } from "@dukkani/common/schemas/product/input";
-import { Card, CardContent } from "@dukkani/ui/components/card";
+import {
+	type VariantOptionInput,
+	variantOptionInputSchema,
+} from "@dukkani/common/schemas/variant/input";
+import { Button } from "@dukkani/ui/components/button";
+import { Card, CardContent, CardFooter } from "@dukkani/ui/components/card";
 import {
 	Collapsible,
 	CollapsibleContent,
@@ -13,9 +18,11 @@ import {
 	FieldDescription,
 	FieldGroup,
 	FieldLegend,
+	FieldSeparator,
 	FieldSet,
 } from "@dukkani/ui/components/field";
 import { Form } from "@dukkani/ui/components/forms/wrapper";
+import { Icons } from "@dukkani/ui/components/icons";
 import { useAppForm } from "@dukkani/ui/hooks/use-app-form";
 import { useSchemaForm } from "@dukkani/ui/hooks/use-schema-form";
 import { useMutation } from "@tanstack/react-query";
@@ -29,6 +36,7 @@ import {
 	useState,
 } from "react";
 import { toast } from "sonner";
+import { z } from "zod";
 import { ProductCategorySection } from "@/components/app/products/product-category-section";
 import { ProductDescriptionSection } from "@/components/app/products/product-description-section";
 import { ProductEssentialsSection } from "@/components/app/products/product-essentials-section";
@@ -60,13 +68,27 @@ export const ProductForm = forwardRef<ProductFormHandle, { storeId: string }>(
 				published: false,
 				storeId,
 				categoryId: "",
-				hasVariants: false,
-				variants: [{ name: "Size", options: ["S", "M", "L", "XL", "XXL"] }],
+				hasVariants: true,
+				variantOptions: [{ name: "Size", values: [{ value: "S" }, { value: "M" }, { value: "L" }] }] as VariantOptionInput[],
 			},
 			onSubmit: async ({ value }) => {
 				console.log(value);
 			},
+			validators: {
+				onChange: z.object({
+					name: z.string().min(1),
+					description: z.string(),
+					price: z.coerce.number<string>().positive(),
+					stock: z.coerce.number<string>().int().min(1),
+					published: z.boolean(),
+					storeId: z.string().min(1),
+					categoryId: z.string(),
+					hasVariants: z.boolean(),
+					variantOptions: z.array(variantOptionInputSchema),
+				}),
+			},
 		});
+
 		const [isCategoryDrawerOpen, setIsCategoryDrawerOpen] = useState(false);
 		const categoriesOptions = useMemo(() => {
 			if (!categories?.length) return [];
@@ -211,115 +233,150 @@ export const ProductForm = forwardRef<ProductFormHandle, { storeId: string }>(
 								Enter the essentials of your product
 							</FieldDescription>
 							<FieldGroup>
-								<formV2.AppField name="name">
-									{(field) => (
-										<field.TextInput
-											label="Name"
-											placeholder="Enter the name of your product"
-										/>
-									)}
-								</formV2.AppField>
-								<formV2.AppField name="description">
-									{(field) => (
-										<field.TextAreaInput
-											label="Description"
-											placeholder="Enter the description of your product"
-										/>
-									)}
-								</formV2.AppField>
-								<formV2.AppField name="stock">
-									{(field) => <field.NumberInput label="Stock" />}
-								</formV2.AppField>
-								<formV2.AppField name="price">
-									{(field) => (
-										<field.NumberInput
-											label="Price"
-											inputMode="decimal"
-											step={0.1}
-											maxPoints={3}
-										/>
-									)}
-								</formV2.AppField>
-								<formV2.AppField name="categoryId">
-									{(field) => (
-										<field.SelectInput
-											label="Category"
-											options={categoriesOptions}
-											onNewOptionClick={() => {
-												console.log("open modal");
-											}}
-										/>
-									)}
-								</formV2.AppField>
-								<formV2.AppField name="hasVariants">
-									{(field) => (
-										<field.SwitchInput
-											label={t("form.options.label")}
-											description={t("form.options.description")}
-										/>
-									)}
-								</formV2.AppField>
-								<formV2.Subscribe
-									selector={(state) => state.values.hasVariants}
-								>
-									{(hasVariants) => (
-										<Collapsible open={hasVariants}>
-											<CollapsibleContent>
-												<FieldSet>
-													<Card className="mb-4">
-														<CardContent>
-															<FieldGroup>
-																<formV2.AppField name="variants" mode="array">
-																	{(field) =>
-																		field.state.value.map(
-																			(variant, variantIndex) => (
-																				<formV2.AppField
-																					name={`variants[${variantIndex}].name`}
-																					key={variant.name}
-																				>
-																					{(field) => (
-																						<FieldGroup>
-																							<field.TextInput
-																								label="Variant Name"
-																								placeholder="Enter the name of your variant"
-																							/>
-																							<formV2.AppField
-																								name={`variants[${variantIndex}].options`}
-																								mode="array"
-																							>
-																								{(field) =>
-																									field.state.value.map(
-																										(option, optionIndex) => (
-																											<formV2.AppField
-																												name={`variants[${variantIndex}].options[${optionIndex}]`}
-																												key={option}
-																											>
-																												{(field) => (
-																													<field.TextInput
-																														label="Variant Value"
-																														placeholder="Enter the value of your variant"
-																													/>
-																												)}
-																											</formV2.AppField>
-																										),
-																									)
-																								}
-																							</formV2.AppField>
-																						</FieldGroup>
-																					)}
-																				</formV2.AppField>
-																			),
-																		)
-																	}
-																</formV2.AppField>
-															</FieldGroup>
-														</CardContent>
-													</Card>
-												</FieldSet>
-											</CollapsibleContent>
-										</Collapsible>
-									)}
-								</formV2.Subscribe>
+								<formV2.AppForm>
+									<formV2.AppField name="name">
+										{(field) => (
+											<field.TextInput
+												label={t("form.name.label")}
+												placeholder={t("form.name.placeholder")}
+											/>
+										)}
+									</formV2.AppField>
+									<formV2.AppField name="description">
+										{(field) => (
+											<field.TextAreaInput
+												label={t("form.description.label")}
+												placeholder={t("form.description.placeholder")}
+											/>
+										)}
+									</formV2.AppField>
+									<formV2.AppField name="stock">
+										{(field) => (
+											<field.NumberInput label={t("form.stock.label")} />
+										)}
+									</formV2.AppField>
+									<formV2.AppField name="price">
+										{(field) => (
+											<field.NumberInput
+												label={t("form.price.label")}
+												inputMode="decimal"
+												step={0.1}
+												maxPoints={3}
+											/>
+										)}
+									</formV2.AppField>
+									<formV2.AppField name="categoryId">
+										{(field) => (
+											<field.SelectInput
+												label={t("form.category.label")}
+												options={categoriesOptions}
+												onNewOptionClick={() => {
+													console.log("open modal");
+												}}
+											/>
+										)}
+									</formV2.AppField>
+									<formV2.AppField name="hasVariants">
+										{(field) => (
+											<field.SwitchInput
+												label={t("form.options.label")}
+												description={t("form.options.description")}
+											/>
+										)}
+									</formV2.AppField>
+									<formV2.Subscribe
+										selector={(state) => state.values.hasVariants}
+									>
+										{(hasVariants) => (
+											<Collapsible open={hasVariants}>
+												<CollapsibleContent>
+													<FieldSet>
+														<Card className="mb-4">
+															<CardContent>
+																<FieldGroup>
+																	<formV2.AppField
+																		name="variantOptions"
+																		mode="array"
+																	>
+																		{(variantOptionsField) =>
+																			variantOptionsField.state.value?.map(
+																				(variantOption, variantOptionIndex) => (
+																					<formV2.AppField
+																						name={`variantOptions[${variantOptionIndex}].name`}
+																						key={variantOption.name}
+																					>
+																						{(field) => (
+																							<FieldGroup>
+																								<div className="flex flex-1 items-center gap-2">
+																									<field.TextInput
+																										label="Variant Name"
+																										srOnlyLabel
+																										placeholder="Enter the name of your variant"
+																									/>
+																									<Button
+																										type="button"
+																										variant="secondary"
+																										size="icon"
+																										onClick={() =>
+																											variantOptionsField.removeValue(
+																												variantOptionIndex,
+																											)
+																										}
+																									>
+																										<Icons.trash className="h-4 w-4" />
+																									</Button>
+																								</div>
+																								<formV2.AppField
+																									name={`variantOptions[${variantOptionIndex}].values`}
+																									mode="array"
+																								>
+																									{(optionsField) => (
+																										<optionsField.ArrayInput as="text-pills" fromKey="value" />
+																									)}
+																								</formV2.AppField>
+																								<FieldSeparator />
+																							</FieldGroup>
+																						)}
+																					</formV2.AppField>
+																				),
+																			)
+																		}
+																	</formV2.AppField>
+																</FieldGroup>
+															</CardContent>
+															<formV2.AppField name="variantOptions">
+																{(variantOptionsField) => (
+																	<CardFooter>
+																		<Button
+																			type="button"
+																			variant="outline"
+																			size="sm"
+																			className="w-full"
+																			onClick={() => {
+																				variantOptionsField.handleChange(
+																					(prev) => [
+																						...(prev ?? []),
+																						{
+																							name: "",
+																							values: [{ value: "" }],
+																						},
+																					],
+																				);
+																			}}
+																		>
+																			<Icons.plus className="mr-2 h-4 w-4" />
+																			{t("form.variants.options.addAnother")}
+																		</Button>
+																	</CardFooter>
+																)}
+															</formV2.AppField>
+														</Card>
+													</FieldSet>
+												</CollapsibleContent>
+											</Collapsible>
+										)}
+									</formV2.Subscribe>
+								</formV2.AppForm>
 							</FieldGroup>
 						</FieldSet>
 					</FieldGroup>
