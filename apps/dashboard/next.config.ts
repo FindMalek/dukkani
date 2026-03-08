d7d908200ee09d3abc991bd5528bbba7ce9a94fe
FindMalek <hi@findmalek.com>
2026-03-08 13:21:42 +0100

    fix(wip): inject 'API' auth

--- apps/dashboard/next.config.ts
+++ apps/dashboard/next.config.ts
@@ -1,3 +1,4 @@
+import { getApiUrl } from "@dukkani/env/get-api-url";
 import type { NextConfig } from "next";
 import createNextIntlPlugin from "next-intl/plugin";
 
@@ -5,13 +6,16 @@ const withNextIntl = createNextIntlPlugin("./src/lib/i18n.ts");
 
 const nextConfig: NextConfig = {
 	reactCompiler: true,
-	transpilePackages: ["@dukkani/ui"],
+	transpilePackages: ["@dukkani/ui", "@dukkani/env"],
 	serverExternalPackages: [
 		"@prisma/client",
 		"pino",
 		"pino-pretty",
 		"thread-stream",
 	],
+	env: {
+		NEXT_PUBLIC_API_URL: getApiUrl(process.env.NEXT_PUBLIC_API_URL ?? ""),
+	},
 };
 
 export default withNextIntl(nextConfig);
